66cb4dca1df63a5c6f2ef4df5c2d96080aac7c0a
vhodgesosu <vhodgesosu@gmail.com>
2024-09-20 13:47:06 -0500

    finshed phone validator project

--- script.js
+++ script.js
@@ -5,11 +5,18 @@ const clearButton = document.getElementById("clear-btn")
 
 const phoneRegex = /^1?\s?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/;
 
+function validatePhoneNumber(phoneNumber) {
+    return phoneRegex.test(phoneNumber);
+}
+
 function promptUser() {
-    if (userInput.value === "") {
-        prompt("Please provide a phone number")
+    const userPhoneNumber = userInput.value
+    if (userPhoneNumber === "") {
+        alert("Please provide a phone number")
+    } else if (validatePhoneNumber(userPhoneNumber)) {
+        resultsDiv.textContent = "Valid US number: " + userPhoneNumber;
     } else {
-        resultsDiv.textContent = userInput.value;
+        resultsDiv.textContent = "Invalid US number: " + userPhoneNumber;
     }
 }
 
